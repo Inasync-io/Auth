087fdddf0e23142e1456bb5491213ef0c3d69a37
HustlrDen <nrogith@yahoo.com>
2025-06-16 14:18:46 +0530

    update cors policy origin

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,8 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: "http://localhost:3000", 
+    // origin: "http://localhost:3000", 
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"], 
     credentials: true             
   }));
